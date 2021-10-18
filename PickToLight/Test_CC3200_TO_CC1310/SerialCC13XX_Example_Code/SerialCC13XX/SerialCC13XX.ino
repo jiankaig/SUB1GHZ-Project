60a9bb82c5a2e8ed2eb1d7ff7f0b85e8216ea7b1
jiankaig <79676173+jiankaig@users.noreply.github.com>
2021-10-18 15:28:32 +0800

    Update SerialCC13XX.ino

--- PickToLight/Test_CC3200_TO_CC1310/SerialCC13XX_Example_Code/SerialCC13XX/SerialCC13XX.ino
+++ PickToLight/Test_CC3200_TO_CC1310/SerialCC13XX_Example_Code/SerialCC13XX/SerialCC13XX.ino
@@ -19,55 +19,85 @@ SerialCC1.Begin(9600);
 myLink.begin();
 
 SerialCC1.Println(myLink.version());
-txPacket.dstAddr[0] = 0xaa;
+//txPacket.dstAddr[0] = 0xaa;
 
 //SerialCC1.Println("Hello");
 //SerialCC1.Print("Starting.");
-   
+
+//TESTing setup
+bReadDone = true;
+txt = "testing...";
 }
 
 
 void loop() {
 
-char ch = SerialCC1.Read();
-if(ch!=0){ 
-  SerialCC1.Print("Ch:");
-  SerialCC1.Println(String(ch));
-  txt += ch;
-  if(ch == '\n')
-    bReadDone = true;
-  
+  char ch = SerialCC1.Read();
+  if(ch!=0){ 
+    SerialCC1.Print("Ch:");
+    SerialCC1.Println(String(ch));
+    txt += ch;
+    if(ch == '\n')
+      bReadDone = true;
+    }
+    
   if(bReadDone){
     bReadDone = false;
     SerialCC1.Print("Txt:");
     SerialCC1.Println(txt);
     sendStatus();
+    txt = "";
     }
-}
+
 }
 
 
 void sendStatus() {
+  //SerialCC1.Print(txt);
+  
+  char d[128]; 
+  txt.toCharArray(d, sizeof(d));
+  memcpy(&txPacket.payload, &d, sizeof(d)); // Copy the String value into the txPacket payload
+  txPacket.len = sizeof(d); // Set the length of the packet
+  
+  //memcpy(&txPacket.payload, &txt, txt.length()); // Copy the String value into the txPacket payload
+  //txPacket.len = txt.length(); // Set the length of the packet
+  
+  
+  txPacket.absTime = EasyLink_ms_To_RadioTime(0); // Transmit immediately
+  EasyLink_Status status = myLink.transmit(&txPacket); //check trasmit status
 
-//SerialCC1.Print(txt);
+  switch(status){
+    case EasyLink_Status_Success:
+      SerialCC1.Print("TX: ");
+      SerialCC1.Println(d);
+      break;
+    case EasyLink_Status_Tx_Error:
+      SerialCC1.Print("TX Error code: ");
+      SerialCC1.Print(String(status));
+      SerialCC1.Print(" (");
+      //SerialCC1.Print(myLink.getStatusString(status));
+      // SerialCC1.println(")");
+      break;
+    case EasyLink_Status_Config_Error:
+      break;
+    case EasyLink_Status_Param_Error:
+      break;
+    case EasyLink_Status_Mem_Error:
+      break;
+    case EasyLink_Status_Cmd_Error: 
+      break;
+    case EasyLink_Status_Rx_Error:
+      break;
+    case EasyLink_Status_Rx_Timeout:
+      break;
+    case EasyLink_Status_Rx_Buffer_Error:
+      break;
+    case EasyLink_Status_Busy_Error:
+      break;
+    case EasyLink_Status_Aborted:
+      break;    
+    
+    }
 
-
-char d[128]; 
-txt.toCharArray(d, sizeof(d));
-memcpy(&txPacket.payload, &d, sizeof(d)); // Copy the String value into the txPacket payload
-txPacket.len = sizeof(d); // Set the length of the packet
-txPacket.absTime = EasyLink_ms_To_RadioTime(0); // Transmit immediately
-EasyLink_Status status = myLink.transmit(&txPacket); //check trasmit status
-
-if (status == EasyLink_Status_Success) {
-    //SerialCC1.Print("TX: ");
-    //SerialCC1.Println(d);
-} 
-else{
-   SerialCC1.Print("TX Error code: ");
-   SerialCC1.Print(String(status));
-   SerialCC1.Print(" (");
-   //SerialCC1.Print(myLink.getStatusString(status));
-  // SerialCC1.println(")");
-}
 }
