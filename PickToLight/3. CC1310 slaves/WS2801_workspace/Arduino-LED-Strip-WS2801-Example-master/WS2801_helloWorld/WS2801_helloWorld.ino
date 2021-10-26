56cdf5496dc36df9e06d91d0d946a3bad378931e
jiankaig <79676173+jiankaig@users.noreply.github.com>
2021-10-26 18:11:18 +0800

    update WS2801_helloworld
    not functional due to complex library

--- PickToLight/3. CC1310 slaves/WS2801_workspace/Arduino-LED-Strip-WS2801-Example-master/WS2801_helloWorld/WS2801_helloWorld.ino
+++ PickToLight/3. CC1310 slaves/WS2801_workspace/Arduino-LED-Strip-WS2801-Example-master/WS2801_helloWorld/WS2801_helloWorld.ino
@@ -1,164 +1,142 @@
-#include <SPI.h>
-#include <math.h>
-//#include "ColorClass.h"
+//donwload and include library in energia from:
+//https://github.com/adafruit/Adafruit-WS2801-Library
+#include "Adafruit_WS2801.h"
+//#include "SPI.h" // Comment out this line if using Trinket or Gemma
+#ifdef __AVR_ATtiny85__
+ #include <avr/power.h>
+#endif
 
-// basic example for LED Strip WS2801
-// CLK <-> PIN 13
-// SI  <-> PIN 11
+/*****************************************************************************
+Example sketch for driving Adafruit WS2801 pixels!
+  Designed specifically to work with the Adafruit RGB Pixels!
+  12mm Bullet shape ----> https://www.adafruit.com/products/322
+  12mm Flat shape   ----> https://www.adafruit.com/products/738
+  36mm Square shape ----> https://www.adafruit.com/products/683
+  These pixels use SPI to transmit the color data, and have built in
+  high speed PWM drivers for 24 bit color per pixel
+  2 pins are required to interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+*****************************************************************************/
 
-/*****************
-   CONFIGURATION
- *****************/
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+// The colors of the wires may be totally different so
+// BE SURE TO CHECK YOUR PIXELS TO SEE WHICH WIRES TO USE!
+uint8_t dataPin  = 9;    // Yellow wire on Adafruit Pixels
+uint8_t clockPin = 10;    // Green wire on Adafruit Pixels
 
-//SPISettings spiSettings(1000000 /* 1Mhz */, MSBFIRST, SPI_MODE0);
-//#define _SERIAL_ Serial // Serial1 for Nucleo Board
-#define LEDS_COUNT 32
-#define SPEED 25 // 1 ... 60
+// Don't forget to connect the ground wire to Arduino ground,
+// and the +5V wire to a +5V supply
 
-/**************
-   LED DRIVER
- **************/
+// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
+Adafruit_WS2801 strip = Adafruit_WS2801(3, dataPin, clockPin);
 
-typedef struct Color {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-} Color;
+// Optional: leave off pin numbers to use hardware SPI
+// (pinout is then specific to each board and can't be changed)
+//Adafruit_WS2801 strip = Adafruit_WS2801(25);
 
-//function prototypes
-Color RGB(uint8_t r, uint8_t g, uint8_t b);
-void leds_setAll(Color color);
-
-Color RGB(uint8_t r, uint8_t g, uint8_t b) {
-  Color result;
-  result.r = r;
-  result.g = g;
-  result.b = b;
-  return result;
-}
-
-/**
-   internal state of led driver
-*/
-Color _leds_colors[LEDS_COUNT];
-
-/**
-   set the color for all LEDs used on next leds_commit()
-
-   @param Color color to show
-*/
-void leds_setAll(Color color) {
-  for (int i = 0; i < LEDS_COUNT; i++)  {
-    leds_set(i, color);
-  }
-}
-
-/**
-   set the color for a LED used on next leds_commit()
-
-   @param index zero-based index of the LED
-   @param Color color to show
-*/
-void leds_set(int index, Color color) {
-  if (index >= 0 && index < LEDS_COUNT) {
-    Serial.print("setting LED ");
-    Serial.print(index);
-    Serial.print(" to (");
-    Serial.print(color.r);
-    Serial.print(", ");
-    Serial.print(color.g);
-    Serial.print(", ");
-    Serial.print(color.b);
-    Serial.println(")");
-    _leds_colors[index] = color;
-  } else {
-    Serial.print("ignoring out-of-bounds index ");
-    Serial.println(index);
-  }
-}
-
-/**
-   sends the current colors to the LEDs
-*/
-void leds_commit() {
-  Serial.println("sending colors to LEDs");
-  SPI.beginTransaction(SPISettings(1000000 /* 1Mhz */, MSBFIRST, SPI_MODE0));
-  for (int i = 0; i < LEDS_COUNT; i++)  {
-    Color current = _leds_colors[i];
-    SPI.transfer(current.r);
-    SPI.transfer(current.g);
-    SPI.transfer(current.b);
-  }
-  SPI.endTransaction();
-}
-
-/**********
-   MAIN
- **********/
+// For 36mm LED pixels: these pixels internally represent color in a
+// different format.  Either of the above constructors can accept an
+// optional extra parameter: WS2801_RGB is 'conventional' RGB order
+// WS2801_GRB is the GRB order required by the 36mm pixels.  Other
+// than this parameter, your code does not need to do anything different;
+// the library will handle the format change.  Examples:
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin, WS2801_GRB);
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("setting up...");
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
 
-  leds_setAll(RGB(0, 0, 0));
-  Serial.println("setup complete");
+  strip.begin();
+
+  // Update LED contents, to start they are all 'off'
+  strip.show();
 }
 
-int mode = 0;
-int mode0_currentLED = 0;
-int mode1_currentStep = 0;
-#define MODE1_STEP_COUNT 200
-double mode1_stepSize = 2 * 3.14159265 / MODE1_STEP_COUNT;
-#define MODE1_BRIGHTNESS 1.0 // 0.0 ... 1.0
 
 void loop() {
-  switch (mode) {
-    case 0:
-      loop_mode0();
-      break;
-    case 1:
-      loop_mode1();
-      break;
+  // Some example procedures showing how to display to the pixels
+  
+  colorWipe(Color(255, 0, 0), 50);
+  colorWipe(Color(0, 255, 0), 50);
+  colorWipe(Color(0, 0, 255), 50);
+  rainbow(20);
+  rainbowCycle(20);
+}
+
+void rainbow(uint8_t wait) {
+  int i, j;
+   
+  for (j=0; j < 256; j++) {     // 3 cycles of all 256 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel( (i + j) % 255));
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
   }
 }
 
-/**
-   one light dot moves over LED wire
-*/
-void loop_mode0() {
-  leds_setAll(RGB(0, 0, 0));
-  leds_set(mode0_currentLED++, RGB(0xFF, 0xFF, 0xFF));
-  leds_commit();
-
-  if (mode0_currentLED >= LEDS_COUNT) {
-    mode0_currentLED = 0;
-    mode = 1;
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  
+  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
   }
-
-  delay(3000 / SPEED);
 }
 
-/**
-   all LEDS color shift
-*/
-void loop_mode1() {
-  // see plotted RGB graphs here
-  // http://fooplot.com/#W3sidHlwZSI6MCwiZXEiOiItMTI3KmNvcyg2KngqMiozLjE0LzIwMCkrMTI4IiwiY29sb3IiOiIjRjcwNTA1In0seyJ0eXBlIjowLCJlcSI6Ii0xMjcqY29zKDQqeCoyKjMuMTQvMjAwKSsxMjgiLCJjb2xvciI6IiMwMEZDMDAifSx7InR5cGUiOjAsImVxIjoiLTEyNypjb3MoeCoyKjMuMTQvMjAwKSsxMjgiLCJjb2xvciI6IiMwNTA1RkMifSx7InR5cGUiOjEwMDAsIndpbmRvdyI6WyIwIiwiMjAwIiwiMCIsIjI2MCJdfV0-
-  Color color = RGB(
-                  mode1_getColor(6 * mode1_currentStep),
-                  mode1_getColor(4 * mode1_currentStep),
-                  mode1_getColor(1 * mode1_currentStep)
-                );
-  leds_setAll(color);
-  leds_commit();
-  mode1_currentStep += 1;
-  if (mode1_currentStep >= MODE1_STEP_COUNT) {
-    mode1_currentStep = 0;
-    mode = 0;
+// fill the dots one after the other with said color
+// good for testing purposes
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+  
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
   }
-  delay(300 / SPEED);
 }
 
-uint8_t mode1_getColor(int currentStep) {
-  return (uint8_t) (MODE1_BRIGHTNESS * (-127 * cos(currentStep * mode1_stepSize) + 128));
+/* Helper functions */
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
