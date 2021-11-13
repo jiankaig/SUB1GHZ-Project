dd3c03476f32d2affef2bb2ec83138c076fbcf89
gnoteey <89832783+gnoteey@users.noreply.github.com>
2021-11-13 14:54:28 +0800

    update

--- PickToLight/2. CC1310 master/CC1310_MultiThread/Task1.ino
+++ PickToLight/2. CC1310 master/CC1310_MultiThread/Task1.ino
@@ -1,9 +1,30 @@
+EasyLink_TxPacket txPacket;
+
 void setupTask1() {
   // put your setup code here, to run once:
-  
+  SerialCC1.Begin(9600);
+  myLink.begin();
 }
 
 void loopTask1() {
   // put your main code here, to run repeatedly: 
-  
+  com_CC3200toCC1310();
+  if (bReadDone) {
+        bReadDone = false;
+        txStatus(txt); // send Status via Easylink/Sub1Ghz
+        txt = "";
+        bRecDone = false;
+  }
+}
+
+
+void com_CC3200toCC1310()
+{
+  char ch = SerialCC1.Read();
+  //read only when there is something
+    if(ch != 0) {
+      txt += ch;
+      if (ch == '\n')
+        bReadDone = true;
+    }
 }
