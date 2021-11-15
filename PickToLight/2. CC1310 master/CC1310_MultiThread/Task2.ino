e115e3e1e641a4a0b10943ba46967602fae90b83
gnoteey <89832783+gnoteey@users.noreply.github.com>
2021-11-15 13:54:48 +0800

    multi

--- PickToLight/2. CC1310 master/CC1310_MultiThread/Task2.ino
+++ PickToLight/2. CC1310 master/CC1310_MultiThread/Task2.ino
@@ -1,52 +1,4 @@
-<<<<<<< HEAD
-//
-//EasyLink_RxPacket rxPacket;
-//String strValue = "";
-//
-//void setupTask2() {
-//  // put your setup code here, to run once:
-//  Serial.begin(9600);
-//  myLink2.begin();
-//}
-//
-//void loopTask2() {
-//  // put your main code here, to run repeatedly: 
-//  // 1. keep receving from easyline,
-//  // 2. if message received, then send uart message once and,
-//  // 3. back to step 1
-//
-//
-//  char f[128];
-//  // rxTimeout is in Radio time and needs to be converted from miliseconds to Radio Time
-//  rxPacket.rxTimeout = EasyLink_ms_To_RadioTime(200);
-//  // Turn the receiver on immediately
-//  rxPacket.absTime = EasyLink_ms_To_RadioTime(0);
-//  
-//  EasyLink_Status status = myLink2.receive(&rxPacket);
-//  
-//  if (status == EasyLink_Status_Success) {
-//    //memcpy(&value, &rxPacket.payload, sizeof(uint16_t));
-//    memcpy(&f, &rxPacket.payload, sizeof(f));
-//    //SerialCC1.print("Packet received with lenght ");
-//    //SerialCC1.print(rxPacket.len);
-//    //SerialCC1.print(" and value ");
-//    Serial.print(f);
-//    //bRecDone = true;
-//    //SerialCC1.Println(f); //value
-//    //bReadDone = true;
-//    strValue = f;
-//
-//  } else {
-//
-//     //SerialCC1.Println("Error receiving packet with status code: ");
-////    Serial.print(status);
-////      Serial.print(" (");
-////      Serial.print(myLink.getStatusString(status));
-////      Serial.println(")");
-//  }
-//  
-//}
-=======
+
 
 EasyLink_RxPacket rxPacket;
 String strValue = "";
@@ -66,45 +18,42 @@ void loopTask2() {
 
 
   char f[128];
-//  // rxTimeout is in Radio time and needs to be converted from miliseconds to Radio Time
-//  rxPacket.rxTimeout = EasyLink_ms_To_RadioTime(200);
-//  // Turn the receiver on immediately
-//  rxPacket.absTime = EasyLink_ms_To_RadioTime(0); 
-//  EasyLink_Status status = myLink.receive(&rxPacket);
+  // rxTimeout is in Radio time and needs to be converted from miliseconds to Radio Time
+  rxPacket.rxTimeout = EasyLink_ms_To_RadioTime(200);
+  // Turn the receiver on immediately
+  rxPacket.absTime = EasyLink_ms_To_RadioTime(0); 
+  EasyLink_Status status = myLink.receive(&rxPacket);
  
-  EasyLink_Status status = myLink.receive(EasyLink_ReceiveCb_function);
+  //EasyLink_Status status = myLink.receive(EasyLink_ReceiveCb_function);
   
   if (status == EasyLink_Status_Success) {
-    //memcpy(&value, &rxPacket.payload, sizeof(uint16_t));
+//
     memcpy(&f, &rxPacket.payload, sizeof(f));
-    //SerialCC1.print("Packet received with lenght ");
-    //SerialCC1.print(rxPacket.len);
-    //SerialCC1.print(" and value ");
+//
     Serial.print(f);
-    //bRecDone = true;
-    //SerialCC1.Println(f); //value
-    //bReadDone = true;
-    strValue = f;
+//
+//    //bReadDone = true;
+//    strValue = f;
 
-  } else {
+//  } else {
 
-     //SerialCC1.Println("Error receiving packet with status code: ");
-//    Serial.print(status);
+//      SerialCC1.Println("Error receiving packet with status code: ");
+//      Serial.print(status);
 //      Serial.print(" (");
 //      Serial.print(myLink.getStatusString(status));
 //      Serial.println(")");
+//  }
   }
-  
 }
+
 void EasyLink_ReceiveCb_function(EasyLink_RxPacket * rxPacket, EasyLink_Status status){
 //  char f[128];
   // rxTimeout is in Radio time and needs to be converted from miliseconds to Radio Time
-  rxPacket->rxTimeout = EasyLink_ms_To_RadioTime(200);
+  //rxPacket->rxTimeout = EasyLink_ms_To_RadioTime(200);
   // Turn the receiver on immediately
-  rxPacket->absTime = EasyLink_ms_To_RadioTime(0);
+  //rxPacket->absTime = EasyLink_ms_To_RadioTime(0);
   
-  status = myLink.receive(rxPacket);
+  //status = myLink.receive(rxPacket);
 }
 
   
->>>>>>> 46e1d5ee6cbd33a5952295607f9ba40981586dee
