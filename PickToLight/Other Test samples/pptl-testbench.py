8676b887c0e19897eb38dde77c86511da010671e
jiankaig <79676173+jiankaig@users.noreply.github.com>
2022-04-01 11:47:37 +0800

    Update pptl-testbench.py

--- PickToLight/Other Test samples/pptl-testbench.py
+++ PickToLight/Other Test samples/pptl-testbench.py
@@ -2,7 +2,7 @@ import socket
 ip_Addr = "192.168.18.8" #  Must set to address of cc3200
 port = 55056 
 opened_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-command="AAX00020R200G200B200BB"
+command="AAX00020R000G000B000BB"
 print(command)
-byte_message = bytes(command, "utf-8")
-opened_socket.sendto(byte_message, (ip_Addr, port))
+# byte_message = bytes(command, "utf-8")
+opened_socket.sendto(command, (ip_Addr, port))
