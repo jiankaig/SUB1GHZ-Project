e6b6d8e0fe52786147d380886787b4c791fdd325
jiankaig <79676173+jiankaig@users.noreply.github.com>
2022-04-05 12:27:48 +0800

    Create udpEchoServer.py
    added std in to exit and terminiate properly

---
+++ PickToLight/Other Test samples/udpEchoServer.py
@@ -0,0 +1,55 @@
+import asyncio
+import sys
+
+async def get_steam_reader(pipe) -> asyncio.StreamReader:
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader(loop=loop)
+    protocol = asyncio.StreamReaderProtocol(reader)
+    await loop.connect_read_pipe(lambda: protocol, pipe)
+    return reader
+
+class EchoServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        print('Received %r from %s' % (message, addr))
+        # print('Send %r to %s' % (message, addr))
+        # self.transport.sendto(data, addr)
+    def connection_lost(data, addr):
+        print(f"EchoServerProtocol disconnected:  {data}: {addr}")
+
+async def main():
+    print("Starting UDP server")
+
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
+
+    # One protocol instance will be created to serve all
+    # client requests.
+    transport, protocol = await loop.create_datagram_endpoint(
+        lambda: EchoServerProtocol(),
+        # local_addr=('192.168.18.7', 55007))
+        local_addr=('127.0.0.1', 9999))
+
+    print(f"press any key and enter to exit..")
+    reader = await get_steam_reader(sys.stdin)
+    data = await reader.readline()
+    if(data != 0):
+        print(f"data: {data}")
+        transport.close()
+        quit()
+
+    try:
+        await asyncio.sleep(3600)  # Serve for 1 hour.
+    finally:
+        transport.close()
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
